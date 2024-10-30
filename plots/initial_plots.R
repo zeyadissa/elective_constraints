945769ced9add22edc8ecfa656ed6e424839b91e
Ashley Fernando (tier0) <tier0\ashley.fernando@ec2amaz-fyi4yb.tier0.prd.thf.aws>
2024-10-30 13:44:37 +0000

    added diagnostic graphs and ratios for rtt

--- plots/initial_plots.R
+++ plots/initial_plots.R
@@ -6,6 +6,7 @@ library(ggplot2)
 library(dplyr)
 library(tidyverse)
 library(scales)
+library(Gini)
 
 ########## High level RTT graphs ##############
 
@@ -152,3 +153,102 @@ incomplete_pathways_specialtyindex_graph <- ggplot(incomplete_pathways_specialty
 
 ggsave(filename = "incomplete_pathways_specialtyindex_graph.jpeg", plot = incomplete_pathways_specialtyindex_graph, width = 10, height = 6, dpi = 300)
 
+############## High level diagnostics ###################
+
+diagnostics_overview<-diagnostic_data %>% 
+  group_by(date) %>% 
+  summarise(total = sum(total, na.rm = TRUE))
+
+operating_theatres_overview<-operating_theatre_data %>% 
+  group_by(date) %>% 
+  summarise(operating_theatres = sum(operating_theatres, na.rm = TRUE))
+
+operating_theatres_graph<-ggplot(operating_theatres_overview,aes(x=date,y=operating_theatres))+
+  geom_line()+
+  labs(title = "Total operating theatres",
+       x = "Date",
+       y = "Total operating theatres")+
+  scale_y_continuous(labels = label_number()) +
+  theme_minimal()
+
+ggsave(filename = "operating_theatres_graph.jpeg", plot = operating_theatres_graph, width = 10, height = 6, dpi = 300)
+
+operating_theatres_graph<-ggplot(operating_theatre_data,aes(x=date,y=operating_theatres, group = trust_code))+
+  geom_line()+
+  labs(title = "Total operating theatres",
+       x = "Date",
+       y = "Total operating theatres")+
+  scale_y_continuous(labels = label_number()) +
+  theme_minimal()
+
+########### Ratio of completed pathways to incomplete pathways #############
+
+# Corrected code to calculate completed pathways for admitted and non-admitted patients by date
+elective_ratio <- rtt_data %>%
+  group_by(date) %>%
+  summarise(
+    completed_pathways_admitted = sum(completed_pathways_for_admitted_patients, na.rm = TRUE),
+    completed_pathways_nonadmitted = sum(completed_pathways_for_non_admitted_patients, na.rm = TRUE),
+    incomplete_pathways = sum(incomplete_pathways,na.rm = TRUE)) %>% 
+  mutate(complete_admitted_divided_incomplete_pathways = completed_pathways_admitted/incomplete_pathways)
+
+elective_ratio_graph<-ggplot(elective_ratio,aes(x=date,y=complete_admitted_divided_incomplete_pathways))+
+  geom_line()+
+  labs(title = "Total complete admitted pathways / incomplete pathways",
+       x = "Date",
+       y = "Ratio of complete admitted pathways / incomplete pathways")+
+  scale_y_continuous(labels = label_number()) +
+  theme_minimal()
+
+ggsave(filename = "elective_ratio_graph.jpeg", plot = elective_ratio_graph, width = 10, height = 6, dpi = 300)
+
+elective_ratio_2 <- rtt_data %>%
+  group_by(date,trust_code) %>% 
+  summarise(
+    completed_pathways_admitted = sum(completed_pathways_for_admitted_patients, na.rm = TRUE),
+    completed_pathways_nonadmitted = sum(completed_pathways_for_non_admitted_patients, na.rm = TRUE),
+    incomplete_pathways = sum(incomplete_pathways,na.rm = TRUE)) %>% 
+  mutate(complete_admitted_divided_incomplete_pathways = completed_pathways_admitted/incomplete_pathways)
+
+elective_ratio_2_filtered <- elective_ratio_2 %>%
+  filter(completed_pathways_admitted != 0) %>% 
+  filter(complete_admitted_divided_incomplete_pathways >= 50) %>%
+  filter(is.finite(complete_admitted_divided_incomplete_pathways)) 
+
+
+elective_ratio_trusts_graph <- ggplot(elective_ratio_2_filtered, aes(x = date, 
+                                                                     y = complete_admitted_divided_incomplete_pathways, 
+                                                                     group = trust_code)) +
+  geom_line(show.legend = FALSE) +  # Remove the legend
+  labs(
+    title = "Ratio of Complete Admitted Pathways to Incomplete Pathways by Trust",
+    x = "Date",
+    y = "Ratio of Complete Admitted / Incomplete Pathways"
+  ) +
+  scale_y_continuous(labels = label_number()) +
+  theme_minimal()
+
+ggsave(filename = "elective_ratio_trusts_graph.jpeg", plot = elective_ratio_trusts_graph, width = 10, height = 6, dpi = 300)
+
+
+
+########## Gini index #######
+
+
+Gini = function (x, corr = FALSE, na.rm = TRUE) 
+{
+  if (!na.rm && any(is.na(x))) 
+    return(NA_real_)
+  x <- as.numeric(na.omit(x))
+  l <- length(x)
+  x <- sort(x)
+  gi <- sum(x * 1L:l)
+  gi <- 2 * gi/sum(x) - (l + 1L)
+  if (corr) 
+    gi/(l - 1L)
+  else gi/l
+}
+
+gini_by_trust_specialty <- elective_workforce %>%
+  group_by(org_code, Workforce) %>%
+  summarize(gini = Gini(total_fte, na.rm = TRUE))
